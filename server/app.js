0b0929e466c4349547d7e756ea9a82a2539eebc0
Karan Negi <karannegi.agra@gmail.com>
2025-12-09 16:34:24 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://chatzila.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
